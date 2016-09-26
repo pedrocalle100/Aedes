1bc236912b22e60f7a78f0dcda49898939e1982e
djzedo <pedro.djzedo@gmail.com>
2016-09-26 16:19:04 -0500

    Pagina de Consulta
    Se sube el archivo Consulta.php donde esta la pagina donde se hace la
    consulta y se despliega los resultados.
    
    El archivo php.ini es para que muestre los errores en php de manera mas
    clara.
    
    APP_DEBUG se cambia a true para debugging

---
+++ resources/views/Consulta.php
@@ -0,0 +1,276 @@
+<?php
+
+if (array_key_exists('fechaInicial', $_POST)){
+    
+    $link = mysqli_connect("localhost","cl49-aedes","Bm9W-sc4e","cl49-aedes");
+
+if (mysqli_connect_error()){
+
+    die("There was an error connecting to the database");
+
+    }
+
+    
+/*
+    if ( $_POST['email'] == ''){
+      echo "Please fill in a username";
+        
+    }else if( $_POST['password'] == ''){
+          echo "Please fill in a password";
+          
+      }else{
+      */
+    
+                                       // ('Paris','London');   $link,$name)."'";
+    
+    //SELECT * FROM `casos_dengue` where fechaInicial >= .fechaInicial. and fechaFinal <= fechaFinal. AND Sexo IN (M,F) AND Comuna IN(asd,asd,) 
+    
+    //ST["femenino"])."')";
+    
+    if ( $_POST['fechaInicial'] != ''){
+        
+        echo "<table>";
+        
+        $query = "SELECT * FROM `casos_dengue` WHERE 
+            Fecha >= '"
+            .mysqli_real_escape_string($link, $_POST["fechaInicial"])."'
+            AND Fecha <= '"
+            .mysqli_real_escape_string($link, $_POST["fechaFinal"])."'
+            AND Sexo IN ('"
+            .mysqli_real_escape_string($link, $_POST["masculino"])."','"
+            .mysqli_real_escape_string($link, $_POST["femenino"])."') 
+            AND Comuna IN ('"
+            .mysqli_real_escape_string($link, $_POST["Paris"])."','"
+            .mysqli_real_escape_string($link, $_POST["La Madera"])."','"
+            .mysqli_real_escape_string($link, $_POST["Suarez"])."','"
+            .mysqli_real_escape_string($link, $_POST["La Cumbre"])."','"
+            .mysqli_real_escape_string($link, $_POST["Bellavista"])."','"
+            .mysqli_real_escape_string($link, $_POST["Mirador y Altos de Niquia"])."','"
+            .mysqli_real_escape_string($link, $_POST["Niquia"])."','"
+            .mysqli_real_escape_string($link, $_POST["Fontidueno"])."','"
+            .mysqli_real_escape_string($link, $_POST["Acevedo"])."')";
+    
+        if ($result = mysqli_query($link,$query)){
+        
+            while($row = mysqli_fetch_array($result)){
+                    print_r($row);
+            
+            }   
+            
+        }
+   
+    }
+    
+}
+
+
+?>
+<!--
+<form method="post"> 
+    
+    <p>Fecha Inicio</p>
+    <input type="date" name="dateStart">
+    <p>Fecha Final</p>
+    <input type="date" name="dateEnd">
+    <p>Sexo</p>
+    <input type="password" name="password">
+    <input type="submit" value="Signup">
+    
+
+</form>
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+      
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css" integrity="2hfp1SzUoho7/TsGGGDaFdsuuDL0LX2hnUp6VkX3CUQ2K4K+xjboZdsXyp4oUHZj" crossorigin="anonymous">
+      
+    <style type="text/css">
+        
+        
+        
+    </style>  
+      
+  </head>
+
+    
+<body>
+    <div class="container">
+        
+    <h1>Pregunta al Mosquito</h1>
+        
+    <div id = 'error'>
+    
+    </div>    
+        
+    <div id="message"><? echo $error.$successMessage; ?></div>    
+    
+    <form method="post">
+        <fieldset class="form-group">
+             <div class="row">
+                <div class="col-xs-3">
+                    <label for="fechaInicial">Fecha Inicial</label>
+        
+                    <input type="date" class="form-control" id="fechaInicial" name="fechaInicial">
+                           
+                </div>
+                 
+            </div>
+        
+        </fieldset>
+           
+        <fieldset class="form-group">
+            <div class="row">
+                <div class="col-xs-3">
+                    <label for="fechaFinal">Fecha Final</label>
+        
+                    <input type="date" class="form-control" id="fechaFinal" name="fechaFinal">
+                    
+                </div>
+                
+            </div>
+        
+        </fieldset>
+        
+        <hr/>
+          
+        <p>Sexo</p>
+        <fieldset class="form-check">
+            <label class="form-check-inline">
+  
+            <input class="form-check-input" type="checkbox" id="masculino" value="M" name="masculino"> M
+           
+            </label>
+            
+            <label class="form-check-inline">
+           
+            <input class="form-check-input" type="checkbox" id="femenino" value="F" name="femenino"> F
+            
+            </label>
+
+        </fieldset>
+        
+        <hr/>
+        
+        <p>Seleccionar Comuna(s):</p>        
+        <fieldset class="form-check">
+            <label class="form-check-inline">
+  
+            <input class="form-check-input" type="checkbox" id="Paris" value="Paris"  name="Paris"> Paris
+            </label>
+            
+            <label class="form-check-inline">
+            <input class="form-check-input" type="checkbox" id="La Madera" value="La Madera" name="La Madera">La Madera
+            </label>
+            
+            <label class="form-check-inline">
+            <input class="form-check-input" type="checkbox" id="Santa Ana" value="Santa Ana" name="Santa Ana"> Santa Ana
+            </label>
+            
+            <label class="form-check-inline">
+            <input class="form-check-input" type="checkbox" id="Suarez" value="Suarez" name="Suarez"> Suárez
+            </label>
+            
+            <label class="form-check-inline">
+            <input class="form-check-input" type="checkbox" id="La Cumbre" value="La Cumbre" name="La Cumbre"> La Cumbre
+            </label>
+            
+            <label class="form-check-inline">
+            <input class="form-check-input" type="checkbox" id="Bellavista" value="Bellavista" name="Bellavista"> Bellavista
+            </label>
+            
+            <label class="form-check-inline">
+            <input class="form-check-input" type="checkbox" id="Mirador y Altos de Niquia" value="Mirador y Altos de Niquia" name="Mirador y Altos de Niquia"> Mirador y Altos de Niquía
+            </label>
+            
+            <label class="form-check-inline">
+            <input class="form-check-input" type="checkbox" id="Niquia" value="Niquia" name="Niquia"> Niquia
+            </label>
+            
+            <label class="form-check-inline">
+            <input class="form-check-input" type="checkbox" id="Fontidueno" value="Fontidueno" name="Fontidueno"> Fontidueño
+            </label>
+            
+            <label class="form-check-inline">
+            <input class="form-check-input" type="checkbox" id="Acevedo" value="Acevedo" name="Acevedo"> Acevedo
+            </label>
+
+        </fieldset>
+          
+        <hr/>
+        
+         <p>Seleccione Rango(s) de edad:</p>     
+        <fieldset class="form-check">
+            
+            <label class="form-check-inline">
+            <input class="form-check-input" type="checkbox" id="0a5" value="0a5" name="0a5"> 0 a 5
+            </label>  
+            
+            <label class="form-check-inline">
+            <input class="form-check-input" type="checkbox" id="6a10" value="6a10" name="6a10"> 6 a 10
+            </label>  
+            
+            <label class="form-check-inline">    
+            <input class="form-check-input" type="checkbox" id="11a15" value="11a15" name="11a15"> 11 a 15
+            </label>  
+        
+        </fieldset>   
+        
+          <hr/>
+        
+         <button type="submit" id="submit" class="btn btn-primary">Go!</button>
+        
+    </form>
+            
+    </div>
+
+    
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
+      
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
+      
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js" integrity="VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>
+    
+    <script type="text/javascript">
+        /*
+        $("form").submit(function (e){
+            e.preventDefault();            
+        
+        var error="";
+        
+        if($("#subject").val() == ""){
+            error += "Subject field is required<br>";
+        
+            
+        }
+            
+        if($("#body").val() == ""){
+            error += "Content field is required<br>";
+        }
+        $("#error").html(error);
+        
+        if(error == "" ){
+            
+        }   esle{
+            
+        } 
+            
+             });
+        */
+              
+    </script>
+    
+</body>
+    
+</html>
+
+
